c0189f5830ee6dc37a79abe85311a8015c929819
Ingrid Ines Tchaptchet Nyassi <300140496@monboreal.ca>
2024-11-15 13:45:52 -0500

    ajout des des fichiers initiaux

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiApp1NUMBERSECRET
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
