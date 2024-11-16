1478ffc891cbc3dbc7ad90f915172b147de3adf0
Ingrid Ines Tchaptchet Nyassi <300140496@monboreal.ca>
2024-11-15 19:40:44 -0500

    modification des fichiers jouer

--- JouerPage.xaml.cs
+++ JouerPage.xaml.cs
@@ -1,14 +1,62 @@
 namespace MauiApp1NUMBERSECRET;
 
-public partial class JouerPage : ContentPage
-{
-	public JouerPage()
+public partial class  JouerPage : ContentPage
+{	private readonly Random random = new();
+    readonly int nombreSecret;
+   readonly int MaxNonIncluse;
+    private readonly int tentatives;
+
+    public JouerPage()
 	{
+        MinIncluse = 0;
+        MaxNonIncluse = 100;
 		InitializeComponent();
+		nombreSecret = random.Next(MinIncluse, MaxNonIncluse);
+		NombreLabel.Text = $"Le nombre secret est entre {MinIncluse} et {MaxNonIncluse}";
 	}
 
-    private void DevinerBtn_Clicked(object sender, EventArgs e)
+    public int MinIncluse { get; private set; }
+	 
+	private Task<string> NavigationPopAsync()
+	{
+		throw new NotImplementedException();
+	}
+    private void DisplayAlert(string v1, string v2)
     {
-
+        throw new NotImplementedException();
     }
+    
+    public async void DevinerBtn_Clicked(int nombre)
+		
+	{
+        tentatives++;
+		{
+            if (nombre == nombreSecret)
+            {
+                await DisplayAlert("Secret Number", $"Bingo, you guessed the right number" + $"{nombre} dans {tentatives} tentatives", "Ok");
+                await NavigationPopAsync();
+
+            }
+            else
+            {
+                if (nombre > nombreSecret)
+                {
+                    DisplayAlert("Secret Number", $"The number is less than {nombre}.keep trying, you will get it");
+                    NombreEntry.Text = "";
+
+
+                }
+                else
+                {
+                    DisplayAlert("Secret Number", $"The number is greater than {nombre}. keep trying you will get it");
+                    NombreEntry.Text = "";
+
+                }
+            }
+        }
+
+	}
+
+     
 }
+
